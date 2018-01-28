65d1753f32c13476a4a283f37a26f05ab57caa79
Brian Hogg <brian@hoggworks.com>
2018-01-28 17:32:43 -0500

    Bunch of header progress

--- src/components/DesktopHeader.js
+++ src/components/DesktopHeader.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
@@ -14,41 +14,48 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 
-const DesktopHeader = (props) => {
-  const { user, menu_items } = props;
-  return <div className="header-desktop">
-    <div className="header-desktop__logo">
-      <img src="/images/logo_wordmark.png"/>
+class DesktopHeader extends Component {
+  constructor() {
+    super()
+  }
+
+
+  render() {
+    const { user, menu_items } = this.props;
+    return <div className="header-desktop">
+      <div className="header-desktop__logo">
+        <img src="/images/logo_wordmark.png"/>
+      </div>
+      <div className="header-desktop__links">
+        {menu_items.map((item, index) =>
+          <React.Fragment key={`desktop-menu__item-${index}`}>
+            { (item.logged_in === true && user && user.username) &&
+              <Link
+                to={item.link}
+                >
+                {item.title}
+              </Link>
+            }
+            { (item.logged_in === false && ((user && !user.username) || !user)) && 
+              <Link
+                to={item.link}
+                >
+                {item.title}
+              </Link>
+            }
+            { item.logged_in === undefined && 
+              <Link
+                to={item.link}
+                >
+                {item.title}
+              </Link>
+            }
+            
+          </React.Fragment>
+        )}
+      </div>
     </div>
-    <div className="header-desktop__links">
-      {menu_items.map((item, index) =>
-        <React.Fragment key={`desktop-menu__item-${index}`}>
-          { (item.logged_in === true && user && user.username) &&
-            <Link
-              to={item.link}
-              >
-              {item.title}
-            </Link>
-          }
-          { (item.logged_in === false && ((user && !user.username) || !user)) && 
-            <Link
-              to={item.link}
-              >
-              {item.title}
-            </Link>
-          }
-          { item.logged_in === undefined && 
-            <Link
-              to={item.link}
-              >
-              {item.title}
-            </Link>
-          }
-          
-        </React.Fragment>
-      )}
-    </div>
-  </div>
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(DesktopHeader);
--- src/components/MobileHeader.js
+++ src/components/MobileHeader.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 
@@ -13,55 +13,82 @@ const mapDispatchToProps = (dispatch) => {
   }
 }
 
+class MobileHeader extends Component {
+  constructor() {
+    super()
+    this.state = {
+      showSideMenu: false
+    }
+  }
 
-const MobileHeader = (props) => {
-  
-  const { user, menu_items } = props;
-  return <div className="header-mobile">
-    <div className="header-mobile__strip">
-      <div className="header-mobile__strip-toggle">
-        H
-      </div>
-      <div className="header-mobile__strip-logo">
-        <img src="/images/logo_wordmark.png"/>
-      </div>
-      {(user && user.username) &&
-        <div className="header-mobile__strip-avatar">
-          User!
+
+  toggleSideMenu = () => {
+    const newSideMenuVal = !this.state.showSideMenu
+    this.setState({ showSideMenu: newSideMenuVal })
+  }
+
+
+  render() {
+    const { user, menu_items } = this.props
+    const { showSideMenu } = this.state
+
+    return <div className="header-mobile">
+      <div className="header-mobile__strip">
+        <div
+          className="header-mobile__strip-toggle"
+          onClick={this.toggleSideMenu}>
+          <div className={showSideMenu === true ? "icon" : "icon--hidden"}>
+            <span className="fas fa-times" />
+          </div>
+          <div className={showSideMenu === false ? "icon" : "icon--hidden"}>
+            <span className="fas fa-bars" />
+          </div>
         </div>
-      }
-    </div>
-    <div className="header-mobile__links--wrapper">
-      <div className="header-mobile__links">
-        {menu_items.map((item, index) =>
-          <React.Fragment key={`mobile-menu__item-${index}`}>
-            { (item.logged_in === true && user && user.username) &&
-              <Link
-                to={item.link}
-                >
-                {item.title}
-              </Link>
-            }
-            { (item.logged_in === false && ((user && !user.username) || !user)) && 
-              <Link
-                to={item.link}
-                >
-                {item.title}
-              </Link>
-            }
-            { item.logged_in === undefined && 
-              <Link
-                to={item.link}
-                >
-                {item.title}
-              </Link>
-            }
-            
-          </React.Fragment>
-        )}
+        <div className="header-mobile__strip-logo">
+          <img src="/images/logo_wordmark.png"/>
+        </div>
+        {(user && user.username) &&
+          <div className="header-mobile__strip-avatar">
+            User!
+          </div>
+        }
+      </div>
+      <div
+        className={this.state.showSideMenu === true ? "header-mobile__links-bg header-mobile__links-bg--open" : "header-mobile__links-bg"}
+        onClick={this.toggleSideMenu}>
+      </div>
+      <div className={this.state.showSideMenu === true ? "header-mobile__links--wrapper header-mobile__links--wrapper--open" : "header-mobile__links--wrapper"}>
+        <div className="header-mobile__links">
+          {menu_items.map((item, index) =>
+            <React.Fragment key={`mobile-menu__item-${index}`}>
+              { (item.logged_in === true && user && user.username) &&
+                <Link
+                  to={item.link}
+                  >
+                  {item.title}
+                </Link>
+              }
+              { (item.logged_in === false && ((user && !user.username) || !user)) && 
+                <Link
+                  to={item.link}
+                  >
+                  {item.title}
+                </Link>
+              }
+              { item.logged_in === undefined && 
+                <Link
+                  to={item.link}
+                  >
+                  {item.title}
+                </Link>
+              }
+              
+            </React.Fragment>
+          )}
+        </div>
       </div>
     </div>
-  </div>
+  }
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(MobileHeader);
